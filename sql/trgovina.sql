5d5c5ceb21a1cbad4a33ff86ecd93c61544a85e5
aljaz11 <aj2092@student.uni-lj.si>
2020-11-27 21:09:45 +0100

    baza

--- sql/trgovina.sql
+++ sql/trgovina.sql
@@ -1,18 +1,16 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     19.11.2020 22:41:36                          */
+/* Created on:     25.11.2020 11:32:31                          */
 /*==============================================================*/
 
 
 drop table if exists Artikel;
 
+drop table if exists Kolicina;
+
 drop table if exists Naročilo;
 
 drop table if exists Uporabnik;
-
-drop table if exists Vloga;
-
-drop table if exists sestavlja;
 
 /*==============================================================*/
 /* Table: Artikel                                               */
@@ -28,13 +26,24 @@ create table Artikel
 );
 
 /*==============================================================*/
+/* Table: Kolicina                                              */
+/*==============================================================*/
+create table Kolicina
+(
+   id_kolicina          int not null,
+   order_id             int not null,
+   article_id           int not null,
+   overal               int not null,
+   primary key (id_kolicina)
+);
+
+/*==============================================================*/
 /* Table: Naročilo                                              */
 /*==============================================================*/
 create table Naročilo
 (
    order_id             int not null,
    costumer_id          int not null,
-   articles             text not null,
    total_price          float not null,
    order_status         int not null,
    primary key (order_id)
@@ -46,7 +55,6 @@ create table Naročilo
 create table Uporabnik
 (
    costumer_id          int not null,
-   vloga_id             int not null,
    name                 text not null,
    surname              text not null,
    street               text not null,
@@ -55,38 +63,16 @@ create table Uporabnik
    post_number          int not null,
    email                text not null,
    geslo                text not null,
+   vloga                text not null,
    primary key (costumer_id)
 );
 
-/*==============================================================*/
-/* Table: Vloga                                                 */
-/*==============================================================*/
-create table Vloga
-(
-   vloga_id             int not null,
-   ime_vloge            text not null,
-   primary key (vloga_id)
-);
+alter table Kolicina add constraint FK_r2 foreign key (order_id)
+      references Naročilo (order_id) on delete restrict on update restrict;
 
-/*==============================================================*/
-/* Table: sestavlja                                             */
-/*==============================================================*/
-create table sestavlja
-(
-   article_id           int not null,
-   order_id             int not null,
-   primary key (article_id, order_id)
-);
-
-alter table Naročilo add constraint FK_odda foreign key (costumer_id)
-      references Uporabnik (costumer_id) on delete restrict on update restrict;
-
-alter table Uporabnik add constraint FK_ima foreign key (vloga_id)
-      references Vloga (vloga_id) on delete restrict on update restrict;
-
-alter table sestavlja add constraint FK_sestavlja foreign key (article_id)
+alter table Kolicina add constraint FK_r3 foreign key (article_id)
       references Artikel (article_id) on delete restrict on update restrict;
 
-alter table sestavlja add constraint FK_sestavlja foreign key (order_id)
-      references Naročilo (order_id) on delete restrict on update restrict;
+alter table Naročilo add constraint FK_r1 foreign key (costumer_id)
+      references Uporabnik (costumer_id) on delete restrict on update restrict;
 
