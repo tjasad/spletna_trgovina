f841056324547b5243790353f61eefe3afd23b1f
aljaz11 <aj2092@student.uni-lj.si>
2020-11-27 23:46:21 +0100

    Dodana orderdDB in articelDB

---
+++ model/ArticelDB.php
@@ -0,0 +1,76 @@
+<?php
+
+require_once "DBInit.php";
+
+class ArticelDB {
+
+    public static function getAll() {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("SELECT article_id, article_name, article_price, article_description, article_status FROM Artikel");
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
+    public static function getArticlesByStatus($status) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("SELECT * FROM Artikel WHERE article_status = :status ");
+        $statement->bindParam(":status", $status, PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
+
+    public static function get($id) {
+
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("SELECT * FROM Artikel
+            WHERE article_id  = :id");
+        $statement->bindParam(":id", $id, PDO::PARAM_INT);
+        $statement->execute();
+
+        $book = $statement->fetch();
+
+        if ($book != null) {
+            return $book;
+        } else {
+            throw new InvalidArgumentException("No record with id $id");
+        }
+    }
+    public static function insert($article_id, $article_name, $article_price, $article_description, $article_status) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("INSERT INTO Artikel (article_id, article_name, article_price, article_description, article_status)
+            VALUES (:article_id, :article_name, :article_price, :article_description, :article_status)");
+        $statement->bindParam(":article_id", $article_id);
+        $statement->bindParam(":article_name", $article_name);
+        $statement->bindParam(":article_price", $article_price);
+        $statement->bindParam(":article_description", $article_description);
+        $statement->bindParam(":article_status", $article_status);       
+        $statement->execute();
+    }
+    public static function update($article_id, $article_name, $article_price, $article_description, $article_status) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("UPDATE Artikel SET article_name = :article_name, article_price = :article_price,
+        article_description = :article_description, article_status = :article_status WHERE article_id = :article_id");
+        $statement->bindParam(":article_id", $article_id);
+        $statement->bindParam(":article_name", $article_name);
+        $statement->bindParam(":article_price", $article_price);
+        $statement->bindParam(":article_description", $article_description);
+        $statement->bindParam(":article_status", $article_status);
+        $statement->execute();
+    }
+    public static function delete($id) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("DELETE FROM Artikel WHERE article_id = :id");
+        $statement->bindParam(":id", $id, PDO::PARAM_INT);
+        $statement->execute();
+    }   
+
+  
+
+}
---
+++ model/OrderDB.php
@@ -0,0 +1,92 @@
+<?php
+
+require_once "DBInit.php";
+
+class OrderDB {
+
+    public static function getAll() {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("SELECT order_id, costumer_id, total_price , order_status FROM Naročilo");
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
+    public static function getOrdersByStatus($status) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("SELECT * FROM Naročilo WHERE order_status = :status ");
+        $statement->bindParam(":status", $status, PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetchAll();
+    }
+
+    public static function get($id) {
+
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("SELECT * FROM Naročilo
+            WHERE order_id  = :id");
+        $statement->bindParam(":id", $id, PDO::PARAM_INT);
+        $statement->execute();
+
+        $book = $statement->fetch();
+
+        if ($book != null) {
+            return $book;
+        } else {
+            throw new InvalidArgumentException("No record with id $id");
+        }
+    }
+    public static function get_all_articles_from_order($orderID) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("SELECT a.article_id, a.article_name, a.article_price, a.article_description, a.article_status
+            FROM Naročilo n, Kolicina k, Artikel a
+            WHERE a.article_id = k.article_id AND k.order_id = n.order_id");
+         $statement->bindParam(":id", $id, PDO::PARAM_INT);
+         $statement->execute();
+ 
+         $book = $statement->fetch();
+ 
+         if ($book != null) {
+             return $book;
+         } else {
+             throw new InvalidArgumentException("No record with id $id");
+         }
+
+    }
+    public static function insert($order_id, $costumer_id, $total_price, $order_status) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("INSERT INTO Naročilo (order_id, costumer_id, total_price, order_status)
+            VALUES (:order_id, :costumer_id, :total_price, :order_status)");
+        $statement->bindParam(":order_id", $order_id);
+        $statement->bindParam(":costumer_id", $costumer_id);
+        $statement->bindParam(":total_price", $total_price);
+        $statement->bindParam(":order_status", $order_status);      
+        $statement->execute();
+    }
+    public static function update($order_id, $costumer_id, $total_price, $order_status) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("UPDATE Naročio SET costumer_id = :costumer_id, total_price = :total_price,
+        order_status = :order_status WHERE order_id = :order_id");
+        $statement->bindParam(":order_id", $order_id);
+        $statement->bindParam(":costumer_id", $costumer_id);
+        $statement->bindParam(":total_price", $total_price);
+        $statement->bindParam(":order_status", $order_status);
+        $statement->execute();
+    }
+    public static function delete($id) {
+        $db = DBInit::getInstance();
+
+        $statement = $db->prepare("DELETE FROM Naročilo WHERE order_id = :id");
+        $statement->bindParam(":id", $id, PDO::PARAM_INT);
+        $statement->execute();
+    }   
+
+  
+
+}
