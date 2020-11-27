280ab4031e38f0cfbaac3aef0f9da6cd36dba95f
aljaz11 <aj2092@student.uni-lj.si>
2020-11-27 22:12:48 +0100

    baza posodobljena

--- sql/trgovina.sql
+++ sql/trgovina.sql
@@ -2,11 +2,11 @@
 /* DBMS name:      MySQL 5.0                                    */
 /* Created on:     25.11.2020 11:32:31                          */
 /*==============================================================*/
-
 drop database if exists trgovina;
 create database trgovina;
 use trgovina;
 
+drop table if exists Artikel;
 
 drop table if exists Kolicina;
 
@@ -64,8 +64,8 @@ create table Uporabnik
    post                 text not null,
    post_number          int not null,
    email                text not null,
-   geslo                text not null,
-   vloga                text not null,
+   password             text not null,
+   role                 text not null,
    primary key (costumer_id)
 );
 
@@ -77,7 +77,17 @@ alter table Kolicina add constraint FK_r3 foreign key (article_id)
 
 alter table Naročilo add constraint FK_r1 foreign key (costumer_id)
       references Uporabnik (costumer_id) on delete restrict on update restrict;
+      
+insert into Artikel (article_id, article_name, article_price, article_description,  article_status) values
+(1, "Krajnska klobasa", 3.45, "Dimljena klobasa", TRUE),
+(2, "Kekec pašteta", 1.34, "Pašteta, ki ni iz Kekca", TRUE),
+(3, "Pisalo modro", 0.78, "Pisalo za pisanje - modro", FALSE),
+(4, "Žemlja bela", 0.25, "Bela žemlja, velika", FALSE),
+(5, "Računalnik", 794.91, "Računalnik - PC, HP", TRUE);
+
+INSERT INTO `Uporabnik` (costumer_id, name, surname, street, house_number,post,post_number,email,password,role) VALUES
 
 (1, 'Ivan', 'Bratko', 'Vecna pot',113,'Ljubljana',1000,'ivan@gmail.com','alala','administrator'),
 (2, 'Mici', 'Luna', 'Vecna pot',115,'Ljubljana',1000,'mici@gmail.com','1234','prodajalec'),
 (3, 'Jo', 'Kip', 'Vesela cesta',1,'Ljubljana',1000,'jo@gmail.com','jooo','stranka');
+
